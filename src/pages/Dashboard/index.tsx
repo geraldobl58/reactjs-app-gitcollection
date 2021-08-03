7fc7099ff705ca44f9cf9eb6f52ae55b2f48995b
Geraldo Luiz <geraldobl58@gmail.com>
2021-08-03 10:50:20 -0300

    :sparkles: Creating dashboard page

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,40 @@
 import React from 'react';
 
-import { Title } from './styles';
+import { FiChevronRight } from 'react-icons/fi';
+
+import { Title, Form, Repos } from './styles';
+
+import logo from '../../assets/logo.svg';
 
 export const Dashboard: React.FC = () => {
-  return <Title>Dashboard</Title>;
+  return (
+    <>
+      <img src={logo} alt="GitCollection" />
+      <Title>Catálogo de repositórios do Github</Title>
+
+      <Form>
+        <input placeholder="username/repository_name" />
+        <button type="submit">Buscar</button>
+      </Form>
+
+      <Repos>
+        <a href="/repositories">
+          <img
+            src="https://github.githubassets.com/images/modules/logos_page/Octocat.png"
+            alt="Repositórios"
+          />
+          <div>
+            <strong>geraldobl58/wongames-client</strong>
+            <p>
+              Este é um boilerplate Next.js que foi desenvolvido no curso de
+              React Avançado.
+            </p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repos>
+    </>
+  );
 };
 
 export default Dashboard;
