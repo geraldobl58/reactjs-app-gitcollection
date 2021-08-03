6aae3488cab1be98bebe421abf91224c18728b7c
Geraldo Luiz <geraldobl58@gmail.com>
2021-08-03 11:30:02 -0300

    :sparkles: Consuming Github API

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,37 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { FiChevronRight } from 'react-icons/fi';
 
 import { Title, Form, Repos } from './styles';
 
 import logo from '../../assets/logo.svg';
+import { api } from '../../services/api';
+
+export type GithubRepositoryProps = {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+};
 
 export const Dashboard: React.FC = () => {
+  const [repository, setRepository] = useState<GithubRepositoryProps[]>([]);
+  const [newRepository, setNewRepository] = useState('');
+
+  function handleInputChange(event: React.ChangeEvent<HTMLInputElement>): void {
+    setNewRepository(event.target.value);
+  }
+
+  async function handleAddRepository(
+    event: React.FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault();
+    const response = await api.get<GithubRepositoryProps>(
+      `/repos/${newRepository}`,
+    );
+
+    const repositories = response.data;
+
+    setRepository([...repository, repositories]);
+    setNewRepository('');
+  }
+
   return (
     <>
       <img src={logo} alt="GitCollection" />
       <Title>Catálogo de repositórios do Github</Title>
 
-      <Form>
-        <input placeholder="username/repository_name" />
+      <Form onSubmit={handleAddRepository}>
+        <input
+          placeholder="username/repository_name"
+          onChange={handleInputChange}
+        />
         <button type="submit">Buscar</button>
       </Form>
 
       <Repos>
-        <a href="/repositories">
-          <img
-            src="https://github.githubassets.com/images/modules/logos_page/Octocat.png"
-            alt="Repositórios"
-          />
-          <div>
-            <strong>geraldobl58/wongames-client</strong>
-            <p>
-              Este é um boilerplate Next.js que foi desenvolvido no curso de
-              React Avançado.
-            </p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
+        {repository.map(repo => (
+          <a href="/repositories" key={repo.full_name}>
+            <img src={repo.owner.avatar_url} alt={repo.owner.login} />
+            <div>
+              <strong>{repo.full_name}</strong>
+              <p>{repo.description}</p>
+            </div>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Repos>
     </>
   );
